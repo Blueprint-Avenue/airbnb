6e8faa2458beee57f92a5eced3d4d5ad5e41a2b3
Samiya <skmcintosh20@gmail.com>
2022-06-02 14:40:06 -0700

    completed banner component/css & date picker

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
+import Banner from "./Banner/Banner";
 import "./home.css";
 
 function Home() {
 	return (
 		<div className="home">
 			{/* Banner */}
+			<Banner />
 			{/* Search */}
 			{/* Cards */}
 			{/* Footer */}
-			smile
 		</div>
 	);
 }
---
+++ src/components/Home/Search/Search.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+import "./search.css";
+import {DateRangePicker} from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import {People} from "@material-ui/icons";
+import {Button} from "@material-ui/core";
+
+function Search() {
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+
+	const selectionRange = {
+		startDate: startDate,
+		endDate: endDate,
+		key: "selection",
+	};
+
+	// done by the library itself, just need the function
+	function handleSelectDate(ranges) {
+		setStartDate(ranges.selection.startDate);
+		setEndDate(ranges.selection.endDate);
+	}
+
+	return (
+		<div className="search">
+			<DateRangePicker ranges={[selectionRange]} onChange={handleSelectDate} />
+			<h2>
+				Number of guests
+				<People />
+			</h2>
+			<input min={0} defaultValue={2} type="number" />
+			<Button>Search Airbnb</Button>
+		</div>
+	);
+}
+
+export default Search;
